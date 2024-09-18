c0c9d843ca87215e4722996200fd52190450c872
Omair Khan <omairtkhan@gmail.com>
2024-09-17 23:54:53 -0700

    Added more UI Controls and getting the app to work

--- Project Files/Form1.Designer.cs
+++ Project Files/Form1.Designer.cs
@@ -19,8 +19,18 @@
         {
             this.dataGridViewExpenses = new System.Windows.Forms.DataGridView();
             this.btnAddExpense = new System.Windows.Forms.Button();
+            this.btnEditExpense = new System.Windows.Forms.Button();
+            this.btnDeleteExpense = new System.Windows.Forms.Button();
             this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+
+            // Initialize components properties
 
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExpenses)).BeginInit();
             this.SuspendLayout();
@@ -31,17 +41,8 @@
             this.dataGridViewExpenses.Name = "dataGridViewExpenses";
             this.dataGridViewExpenses.Size = new System.Drawing.Size(776, 250);
             this.dataGridViewExpenses.TabIndex = 0;
-            this.labelDate.AutoSize = true;
 
-            this.labelCategory.AutoSize = true;
-            this.labelAmount.AutoSize = true;
-            this.labelDescription.AutoSize = true;
-            this.labelCategory.Location = new System.Drawing.Point(12, 55);
-            this.labelCategory.Name = "labelCategory";
-            this.labelCategory.Size = new System.Drawing.Size(52, 13);
-            this.labelCategory.TabIndex = 9;
-            this.labelCategory.Text = "Category:";
-
+            // Add Expense Button
             this.btnAddExpense.Location = new System.Drawing.Point(700, 20);
             this.btnAddExpense.Name = "btnAddExpense";
             this.btnAddExpense.Size = new System.Drawing.Size(88, 30);
@@ -50,6 +51,16 @@
             this.btnAddExpense.UseVisualStyleBackColor = true;
             this.btnAddExpense.Click += new System.EventHandler(this.btnAddExpense_Click);
 
+            // Edit Expense Button
+            this.btnEditExpense.Location = new System.Drawing.Point(700, 60);
+            this.btnEditExpense.Name = "btnEditExpense";
+            this.btnEditExpense.Size = new System.Drawing.Size(88, 30);
+            this.btnEditExpense.TabIndex = 2;
+            this.btnEditExpense.Text = "Edit Expense";
+            this.btnEditExpense.UseVisualStyleBackColor = true;
+            this.btnEditExpense.Click += new System.EventHandler(this.btnEditExpense_Click);
+
+            // Delete Expense Button
             this.btnDeleteExpense.Location = new System.Drawing.Point(700, 100);
             this.btnDeleteExpense.Name = "btnDeleteExpense";
             this.btnDeleteExpense.Size = new System.Drawing.Size(88, 30);
@@ -58,6 +69,65 @@
             this.btnDeleteExpense.UseVisualStyleBackColor = true;
             this.btnDeleteExpense.Click += new System.EventHandler(this.btnDeleteExpense_Click);
 
+            // DateTimePicker for date
+            this.dateTimePicker.Location = new System.Drawing.Point(110, 20);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 4;
+
+            // ComboBox for category
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Items.AddRange(new object[] { "Food", "Transport", "Rent", "Shopping", "Health", "Others" });
+            this.comboBoxCategory.Location = new System.Drawing.Point(110, 50);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(200, 21);
+            this.comboBoxCategory.TabIndex = 5;
+
+            // TextBox for amount
+            this.txtAmount.Location = new System.Drawing.Point(110, 80);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(200, 20);
+            this.txtAmount.TabIndex = 6;
+
+            // TextBox for description
+            this.txtDescription.Location = new System.Drawing.Point(110, 110);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(200, 20);
+            this.txtDescription.TabIndex = 7;
+
+            // Label for Date
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(12, 25);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(33, 13);
+            this.labelDate.TabIndex = 8;
+            this.labelDate.Text = "Date:";
+
+            // Label for Category
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(12, 55);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(52, 13);
+            this.labelCategory.TabIndex = 9;
+            this.labelCategory.Text = "Category:";
+
+            // Label for Amount
+            this.labelAmount.AutoSize = true;
+            this.labelAmount.Location = new System.Drawing.Point(12, 85);
+            this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(46, 13);
+            this.labelAmount.TabIndex = 10;
+            this.labelAmount.Text = "Amount:";
+
+            // Label for Description
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(12, 115);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(63, 13);
+            this.labelDescription.TabIndex = 11;
+            this.labelDescription.Text = "Description:";
+
+            // Adding components to the form
             this.Controls.Add(this.dataGridViewExpenses);
             this.Controls.Add(this.btnAddExpense);
             this.Controls.Add(this.btnEditExpense);
@@ -66,7 +136,12 @@
             this.Controls.Add(this.comboBoxCategory);
             this.Controls.Add(this.txtAmount);
             this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelCategory);
+            this.Controls.Add(this.labelAmount);
+            this.Controls.Add(this.labelDescription);
 
+            // Form Settings
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -78,3 +153,4 @@
         }
     }
 }
+
